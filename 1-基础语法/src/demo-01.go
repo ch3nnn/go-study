19f6886ba675f498377e7c9cf48b93733c009c06
chentong-dev <chen.yinren@163.com>
2022-03-13 23:46:04 +0800

    变量声明

---
+++ 1-基础语法/src/demo-01.go
@@ -0,0 +1,19 @@
+/**
+ * @Author: chenTong
+ * @Date: 2022/03/13 23:09
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	var s = "string"
+	const X int8 = 100
+	a1, a2 := 1, "abc"
+
+	fmt.Println(s, X, a1, a2)
+}
+
+// 打印输出
+// string 100 1 abc
