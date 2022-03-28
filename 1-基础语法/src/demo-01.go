b9fbaf776b128ece5183ec0d1346f8c6cb7b3e77
chentong-dev <chen.yinren@163.com>
2022-03-28 22:25:19 +0800

    运算符

--- 1-基础语法/src/demo-01.go
+++ 1-基础语法/src/demo-01.go
@@ -8,11 +8,26 @@ package main
 import "fmt"
 
 func main() {
+
 	var s = "string"
 	const X int8 = 100
 	a1, a2 := 1, "abc"
 
 	fmt.Println(s, X, a1, a2)
+
+	// 格式化字符串 %d 表示整型数字，%s 表示字符串
+	var url = "Code=%d&endDate=%s"
+	sprintf := fmt.Sprintf(url, 123, "2020-12-31")
+	fmt.Println(sprintf)
+
+	// 以下几种类型为nil
+	// var a *int
+	// var a []int
+	// var a map[string] int
+	// var a chan int
+	// var func(string) int
+	// var a error
+
 }
 
 // 打印输出
